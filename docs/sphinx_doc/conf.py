62a7e2cf05a10c03db90321bc3aa426e3dd55932
Dennis Herb <162709231+dehe1011@users.noreply.github.com>
2024-07-17 21:37:44 +0200

    Update conf.py

--- docs/sphinx_doc/conf.py
+++ docs/sphinx_doc/conf.py
@@ -30,6 +30,8 @@ release = '0.1.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+
+import sphinxcontrib
 extensions = [
     'sphinx.ext.mathjax',
     #'matplotlib.sphinxext.plot_directive',
